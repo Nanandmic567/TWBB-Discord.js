2031932080c9827266167f31fd82b6ef57ea6737
Mickey Teerawath Maunsopah <30790322+Nanandmic567@users.noreply.github.com>
2020-12-08 17:28:13 +0700

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -53,9 +53,16 @@ botRem.on('message', message => {
   }
 });
 
+// Displaying real data
 botRem.on('message', message => {
   if (message.content === `${prefix}server`) {
-    message.channel.send(`This server's name is: ${message.guild.name}`);
+    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+  }
+});
+
+botRem.on('message', message => {
+  if (message.content === `${prefix}user-info`) {
+    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
   }
 });
 
